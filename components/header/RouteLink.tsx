dfe3c767e22e041d32a8f84805504b1693bcd497
Freedom Evenden <free.eve@pm.me>
2021-09-14 12:20:36 -0700

    Fixed type error in RouteLink styles

--- components/header/RouteLink.tsx
+++ components/header/RouteLink.tsx
@@ -2,12 +2,16 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { makeStyles } from '@material-ui/core';
 import colors from '../../styles/colors';
+import { Props } from 'next/script';
+
+interface RouteLinkProps extends Props {
+  href: string;
+}
 
 const useStyles = makeStyles(() => ({
   link: {
     fontSize: '30px',
     fontFamily: 'Josefin Slab',
-    fontWeight: '500',
     transition: 'font-size 0.3s ease-in, color 0.3s ease-in',
     margin: '0 10px',
     color: colors.textDark,
@@ -28,18 +32,18 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export default function RouteLink(props) {
+export default function RouteLink({ href, children }: RouteLinkProps) {
   const styles = useStyles();
   const router = useRouter();
 
-  const active = router.pathname === props.href ? ` ${styles.active}` : '';
+  const active = router.pathname === href ? ` ${styles.active}` : '';
   const activeUnderline =
-    router.pathname === props.href ? ` ${styles.activeUnderline}` : '';
+    router.pathname === href ? ` ${styles.activeUnderline}` : '';
 
   return (
     <div>
-      <Link href={props.href}>
-        <a className={`${styles.link}${active} `}>{props.children}</a>
+      <Link href={href}>
+        <a className={`${styles.link}${active} `}>{children}</a>
       </Link>
       <div className={`${styles.underline}${activeUnderline}`} />
     </div>
