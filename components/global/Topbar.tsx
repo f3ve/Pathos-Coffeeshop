f239fdf7a728841532ac6e5baca00c40dfc33b66
Freedom Evenden <free.eve@pm.me>
2022-01-24 11:19:14 -0800

    Added a box shadow on scroll to the topbar

--- components/global/Topbar.tsx
+++ components/global/Topbar.tsx
@@ -6,23 +6,45 @@ import {
   useMediaQuery,
   useScrollTrigger,
 } from '@material-ui/core';
+import * as React from 'react';
 import { Logo, MobileDropdown, NavLinks } from '../header';
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
 
 const useStyle = makeStyles(() => ({
   appBar: {
-    boxShadow: 'none',
+    // boxShadow: 'none',
     backgroundColor: 'white',
   },
 }));
 
-export default function Topbar() {
+function ElevationScroll(props: Props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Topbar(props: Props) {
   const styles = useStyle();
-  const trigger = useScrollTrigger();
   const smallScreen = useMediaQuery('(max-width: 600px)');
 
   return (
     <>
-      <Slide appear={false} direction='down' in={!trigger}>
+      {/*<Slide appear={false} direction='down' in={!trigger}>*/}
+      <ElevationScroll {...props}>
         <AppBar className={styles.appBar} position='fixed'>
           <Toolbar>
             <Logo />
@@ -30,7 +52,8 @@ export default function Topbar() {
             {smallScreen ? <MobileDropdown /> : <NavLinks />}
           </Toolbar>
         </AppBar>
-      </Slide>
+      </ElevationScroll>
+      {/*</Slide>*/}
     </>
   );
 }
